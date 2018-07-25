9adb50793e5f85180a44701504cdf9ba7c98b9eb
HaTran <HaTran@DESKTOP-3GPU4AO>
2018-07-25 07:55:53 +0700

    Checking WebElement_Browser

--- src/selenium_api/Topic_02_Xpath_Css_Locator.java
+++ src/selenium_api/Topic_02_Xpath_Css_Locator.java
@@ -17,19 +17,56 @@ public class Topic_02_Xpath_Css_Locator {
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+
+	}
+
+	@Test
+	public void TC_01_Check_Navigate_Page() {
+
+		/*
+		 * Step 01 - Truy cập vào trang: http://live.guru99.com Step 02 - Kiểm tra title
+		 * của page là: "Home page" Step 03 - Click vào link "My Account" để tới trang
+		 * đăng nhập Step 04 - Click CREATE AN ACCOUNT button để tới trang đăng kí tài
+		 * khoản Step 05 - Back lại trang đăng nhập Step 06 - Kiểm tra url của page đăng
+		 * nhập là: http://live.guru99.com/index.php/customer/account/login/ Step 07 -
+		 * Forward tới trang tạo tài khoản Step 08 - Kiểm tra url của page tạo tài khoản
+		 * là: http://live.guru99.com/index.php/customer/account/create/
+		 */
 		driver.get("http://live.guru99.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Home page");
+
+		driver.findElement(By.xpath("//div[@class='footer']//a[text()= 'My Account']")).click();
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+		driver.navigate().back();
+		// Về lại page login thành công
+		Assert.assertTrue(driver.findElement(By.xpath("//a[@title='Create an Account']")).isDisplayed());
+
+		String loginUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginUrl, "http://live.guru99.com/index.php/customer/account/login/");
+
+		driver.navigate().forward();
+
+		// Về lại register thành công
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='form-validate']")).isDisplayed());
+
+		String registerUrl = driver.getCurrentUrl();
+		Assert.assertEquals(registerUrl, "http://live.guru99.com/index.php/customer/account/create/");
+
 	}
 
 	@Test
 	public void TC_02_Login_Empty() {
-
+		driver.get("http://live.guru99.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//div[@class='footer']//a[contains(text(),'My Account')]")).click();
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("");
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("");
 		driver.findElement(By.xpath("//button[@id='send2']")).click();
-
 		String emailErrorMsg = driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).getText();
 		Assert.assertEquals("This is a required field.", emailErrorMsg);
 
@@ -40,7 +77,8 @@ public class Topic_02_Xpath_Css_Locator {
 
 	@Test
 	public void TC_03_Login_With_Email_Invalid() {
-
+		driver.get("http://live.guru99.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//div[@class='footer']//a[contains(text(),'My Account')]")).click();
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("123434234@12312.123123");
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123434234");
@@ -50,7 +88,8 @@ public class Topic_02_Xpath_Css_Locator {
 
 	@Test
 	public void TC_04_Login_With_Password_Incorrect() {
-
+		driver.get("http://live.guru99.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//div[@class='footer']//a[contains(text(),'My Account')]")).click();
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("automation@gmail.com");
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123");
