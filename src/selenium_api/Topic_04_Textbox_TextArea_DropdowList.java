f966303c886793892b6ce694cb5bb01ed95709a3
HaTran <HaTran@DESKTOP-3GPU4AO>
2018-07-30 22:27:29 +0700

    UPDATE TOPIC 04

--- src/selenium_api/Topic_04_Textbox_TextArea_DropdowList.java
+++ src/selenium_api/Topic_04_Textbox_TextArea_DropdowList.java
@@ -1,5 +1,6 @@
 package selenium_api;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -13,9 +14,11 @@ import org.testng.annotations.Test;
 
 public class Topic_04_Textbox_TextArea_DropdowList {
 	WebDriver driver;
-	By nameTextbox= By.xpath("//a[text()='New Customer']");
+	String name, dob, address, city, pin, phone, email, password;
+	// BY ELEMENT
+	By nameTextbox = By.xpath("//a[text()='New Customer']");
 	By dobTextbox = By.xpath("//input[@id='dob']");
-	By addressTextArea= By.xpath("//textarea[@name='addr']");
+	By addressTextArea = By.xpath("//textarea[@name='addr']");
 	By cityTextbox = By.xpath("//input[@name='city']");
 	By stateTextbox = By.xpath("//input[@name='state']");
 	By pinTextbox = By.xpath("//input[@name='pinno']");
@@ -29,26 +32,36 @@ public class Topic_04_Textbox_TextArea_DropdowList {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
+		// DATA TEST INPUT
+		name = "Automation Test";
+		dob = "1995-01-02";
+		address = "123/ Nguyen van Linh";
+		city = "Ha Noi";
+		pin = "653298";
+		phone = "0123456789";
+		email = "auto" + randomUniqueNumber() + "@gmail.com";
+		password = "123123";
+
 	}
 
 	public void TC_01_DropDowList() throws InterruptedException {
 		driver.get("http://daominhdam.890m.com/");
-		
+
 		Select jobRole = new Select(driver.findElement(By.xpath("//select[@id='job1']")));
-		
+
 		Assert.assertTrue(!jobRole.isMultiple());
-		
+
 		jobRole.selectByVisibleText("Automation Tester");
 		Assert.assertEquals(jobRole.getFirstSelectedOption().getText(), "Automation Tester");
 		Thread.sleep(5000);
-		
+
 		jobRole.selectByValue("manual");
 		Assert.assertEquals(jobRole.getFirstSelectedOption().getText(), "Manual Tester");
-		
+
 		jobRole.selectByIndex(3);
 		Assert.assertEquals(jobRole.getFirstSelectedOption().getText(), "Mobile Tester");
-		
-		int jobItems= jobRole.getOptions().size();
+
+		int jobItems = jobRole.getOptions().size();
 		Assert.assertEquals(jobItems, 5);
 
 	}
@@ -56,24 +69,27 @@ public class Topic_04_Textbox_TextArea_DropdowList {
 	@Test
 	public void TC_02_Textbox_TextArea() {
 		driver.get("http://demo.guru99.com/v4");
-		
+
 		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr145853");
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("henEbar");
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-		
-		
-		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
-		
-		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
-		
-		
 
+		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]"))
+				.isDisplayed());
+
+		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
 
 	}
 
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+
 	}
 
+	public int randomUniqueNumber() {
+		Random rand = new Random();
+		int number = rand.nextInt(999999) + 1;
+		return number;
+
+	}
 }
