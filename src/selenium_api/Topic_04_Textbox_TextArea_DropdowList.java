af8c5c84ccb4ab12e4ab6542a76b2c854b1429fd
ISC-HATTM <ISC-HATTM@W010983116.officesg.local>
2018-07-31 17:46:00 +0700

    Topic 04_1

--- src/selenium_api/Topic_04_Textbox_TextArea_DropdowList.java
+++ src/selenium_api/Topic_04_Textbox_TextArea_DropdowList.java
@@ -14,10 +14,11 @@ import org.testng.annotations.Test;
 
 public class Topic_04_Textbox_TextArea_DropdowList {
 	WebDriver driver;
-	String name, dob, address, city, pin, phone, email, password;
+	String name, dob, address, city, pin, phone, email, password, state, customerID= null, newAddress, newCity;
 	// BY ELEMENT
-	By nameTextbox = By.xpath("//a[text()='New Customer']");
-	By dobTextbox = By.xpath("//input[@id='dob']");
+	By nameTextbox = By.xpath("//input[@name='name']");
+	By genderTextbox= By.xpath("//input[@id='gender']");
+	By dobTextbox = By.xpath("//input[@name='dob']");
 	By addressTextArea = By.xpath("//textarea[@name='addr']");
 	By cityTextbox = By.xpath("//input[@name='city']");
 	By stateTextbox = By.xpath("//input[@name='state']");
@@ -32,15 +33,19 @@ public class Topic_04_Textbox_TextArea_DropdowList {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
-		// DATA TEST INPUT
+		// DATA TEST INPUT VARIABLE
 		name = "Automation Test";
 		dob = "1995-01-02";
-		address = "123/ Nguyen van Linh";
+		address = "123 Nguyen van Linh";
 		city = "Ha Noi";
+		state = "Cam Le";
 		pin = "653298";
 		phone = "0123456789";
 		email = "auto" + randomUniqueNumber() + "@gmail.com";
 		password = "123123";
+		newAddress ="333 Le Lai";
+		newCity = " Ho Chi Minh";
+		
 
 	}
 
@@ -78,12 +83,63 @@ public class Topic_04_Textbox_TextArea_DropdowList {
 				.isDisplayed());
 
 		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
+		
+		// INPUT DATA TO NEW CUSTOMER FORM
+		driver.findElement(nameTextbox).sendKeys(name);
+		driver.findElement(dobTextbox).sendKeys(dob);
+		driver.findElement(addressTextArea).sendKeys(address);
+		driver.findElement(cityTextbox).sendKeys(city);
+		driver.findElement(stateTextbox).sendKeys(state);
+		driver.findElement(pinTextbox).sendKeys(pin);
+		driver.findElement(phoneTextbox).sendKeys(phone);
+		driver.findElement(emailTextbox).sendKeys(email);
+		driver.findElement(passwordTextbox).sendKeys(password);
+		driver.findElement(By.xpath("//input[@name='sub']")).click();
+		
+		// GET DYNAMIC CUSTOMER ID
+		customerID= driver.findElement(By.xpath("//td[text()='Customer ID']/following-sibling::td")).getText();
+		System.out.println("DYNAMIC CUSTOMER ID="+ customerID);
+		
+		//VERIFY CREATE NEW CUSTOMER CUSSESS
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText(), name);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Birthdate']/following-sibling::td")).getText(),dob);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(),address);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(),city);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(),state);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(),pin);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(),phone);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(),email);
+		
+		driver.findElement(By.xpath("//a[text()='Edit Customer']")).click();
+		driver.findElement(By.xpath("//input[@name='cusid']")).sendKeys(customerID);
+		driver.findElement(By.xpath("//input[@name='AccSubmit']")).click();
+		
+		//CHECK 3 FIELDs(NAME/GENDERT/DATE OF BIRTH)disable
+		System.out.println("AttributeValue= "+ driver.findElement(nameTextbox).getAttribute("disable"));
+		//String AttributeValue= driver.findElement(nameTextbox).getAttribute("disable");
+		Assert.assertTrue(driver.findElement(nameTextbox).getAttribute("disable").equals("On"));
+		Assert.assertTrue(driver.findElement(genderTextbox).getAttribute("disabled").equals("On"));
+		Assert.assertFalse(driver.findElement(dobTextbox).getAttribute("disable").equals("On"));
+		
+		
+		//VERIFY CUSTOMER NAME/ ADDRESS EQUAL INPUT DATA
+		Assert.assertEquals(driver.findElement(nameTextbox).getAttribute("value"), name);
+		Assert.assertEquals(driver.findElement(addressTextArea).getText(),address);
+		
+		// EDIT DATA FOR ADDRESS/ CITY
+		driver.findElement(addressTextArea).sendKeys(newAddress);
+		driver.findElement(cityTextbox).sendKeys(newCity);
+		driver.findElement(By.xpath("//input[@name='sub']")).click();
+		
+		//VERIFY EDIT CUSTOMER SUCCESS
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(),newAddress);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(),newCity);
 
 	}
 
 	@AfterClass
 	public void afterClass() {
-
+		driver.quit();
 	}
 
 	public int randomUniqueNumber() {
