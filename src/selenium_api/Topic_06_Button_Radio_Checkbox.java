888abb62e948330ed26ef2e0972b9ab085109d70
ISC-HATTM <ISC-HATTM@W010983116.officesg.local>
2018-08-07 17:31:48 +0700

    Checking_Topic06_Buttion_Radio_Checkbox

--- src/selenium_api/Topic_06_Button_Radio_Checkbox.java
+++ src/selenium_api/Topic_06_Button_Radio_Checkbox.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -36,7 +37,7 @@ public class Topic_06_Button_Radio_Checkbox {
 		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='form-validate']")).isDisplayed());
 		Assert.assertEquals(driver.getCurrentUrl(), "http://live.guru99.com/index.php/customer/account/create/");
 	}
-	
+
 	public void TC_02_Custom_Checkbox() throws Exception {
 		driver.get("https://demos.telerik.com/kendo-ui/styling/checkboxes");
 
@@ -68,27 +69,73 @@ public class Topic_06_Button_Radio_Checkbox {
 		clickElementByJavascript(dualZoneCheckbox);
 		Thread.sleep(5000);
 		Assert.assertTrue(isElementSelected(dualZoneCheckbox));
-		
 
 		// Sau khi checkbox đã được chọn - deselect nó và kiểm tra nó chưa được chọn
 		unClickCheckbox(dualZoneCheckbox);
 		Assert.assertFalse(isElementSelected(dualZoneCheckbox));
 	}
 
-	@Test
 	public void TC_03_Custom_Radionbutton() throws Exception {
 		driver.get("http://demos.telerik.com/kendo-ui/styling/radios");
-		/*driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.manage().window().maximize();*/
 		String petrol147kWRadionbutton = "//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input";
 		clickElementByJavascript(petrol147kWRadionbutton);
 		Assert.assertTrue(isElementSelected(petrol147kWRadionbutton));
 	}
 
-	
-	public void TC_04() {
+	public void TC_04_Alert_Sucess() {
+		driver.get("http://daominhdam.890m.com/#");
+		// JS Alert
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		
+		// Thông qua swithTo giống như WebDriver driver.close/quit
+		Alert alert = driver.switchTo().alert();
+		String alertJSMEessage = alert.getText();
+		Assert.assertTrue(alertJSMEessage.equals("I am a JS Alert"));
+		// Accept alert-> Click 0k
+		alert.accept();
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@id='result']")).getText(), "You clicked an alert successfully");
+	}
+
+	public void TC_05_Alert_Cancel() {
+		driver.get("http://daominhdam.890m.com/#");
+		// JS Alert
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		String alertJSMEssageCancel = alert.getText();
+		Assert.assertTrue(alertJSMEssageCancel.equals("I am a JS Confirm"));
+		// Cancel alert
+		alert.dismiss();
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@id='result']")).getText(), "You clicked: Cancel");
 
 	}
+
+	@Test
+	public void TC_06_Alert_Entered() throws Exception {
+		/*
+		 * Step 01 - Truy cập vào trang: http://daominhdam.890m.com/ 
+		 * Step 02 - Click vào button: Click for JS Prompt 
+		 * Step 03 - Verify message hiển thị trong alert là: I am a JS prompt 
+		 * Step 04 - Nhập vào text bất kì (daominhdam) và verify message hiển thị tại Result là: You entered: daominhdam
+		 */
+		driver.get("http://daominhdam.890m.com/#");
+		// JS Alert
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		String alertJSMEssage = alert.getText();
+		Assert.assertTrue(alertJSMEssage.equals("I am a JS prompt"));
+		Thread.sleep(4000);
+		// Sendkey to alert
+		alert.sendKeys("daominhdam");
+		alert.accept();
+		Assert.assertEquals(driver.findElement(By.xpath("//p[@id='result']")).getText(),"You entered: daominhdam");
+	
+
+	}
+
+	/*// Get text in alert
+	String alertText = alert.getText();*/
 
 	@AfterClass
 	public void afterClass() {
