bf87c13d816ee8026465c4dbaef806a5c0bd2e2b
Ha <Ha@Ha-PC>
2018-08-14 22:50:38 +0700

    check topic 081

--- src/selenium_api/Topic_08_Iframe_Windows.java
+++ src/selenium_api/Topic_08_Iframe_Windows.java
@@ -19,7 +19,7 @@ public class Topic_08_Iframe_Windows {
 	@BeforeClass
 	  public void beforeClass() {
 		  driver= new FirefoxDriver();	
-		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		  driver.manage().window().maximize();
 		  
 	  }
@@ -34,6 +34,7 @@ public class Topic_08_Iframe_Windows {
 	  // Step 02: Check close pop up displayed(exist click close icon)
 	  List<WebElement> notificationIfrmage = driver.findElements(By.xpath("//iframe[@id='vizury-notification-template']"));
 	  System.out.println("Number element = "+ notificationIfrmage.size());
+	  
 	  if(notificationIfrmage.size()>0) {
 	  driver.switchTo().frame(notificationIfrmage.get(0));
 	  WebElement closeIcon = driver.findElement(By.xpath("//div[@id='div-close']"));
@@ -42,13 +43,42 @@ public class Topic_08_Iframe_Windows {
 	  JavascriptExecutor js=(JavascriptExecutor) driver;
 	  js.executeScript("arguments[0].click();", closeIcon);
 	  System.out.println("Print close popup");
+	  
+	  /* Issuse 03: Switch between 2 iframe (Switch to default content)-----*/
+	  // Switch to Top Window
+	  driver.switchTo().defaultContent();
+	  
 	  }
+	  /* Issuse 02: Handle dynamic iframe (id: random when page reload....)*/
 	  // Step 03: Verify đoạn text được hiển thị:  What are you looking for?
 	  WebElement lookingForIframe = driver.findElement(By.xpath("//div[@class='flipBannerWrap']//iframe"));
 	  driver.switchTo().frame(lookingForIframe);
+	  
 	  // Check text displayed
 	  String lookingForText = driver.findElement(By.xpath("//span[@id='messageText']")).getText();
-	  Assert.assertEquals("lookingForText", "What are you looking for?");
+	  Assert.assertEquals(lookingForText, "What are you looking for?");
+	  
+	  // Switch to default content
+	  driver.switchTo().defaultContent();
+	  
+	  // Step 04-  Switch to banner iframe
+	  WebElement bannerIframe = driver.findElement(By.xpath("//div[@class='slidingbanners']//iframe"));
+	  driver.switchTo().frame(bannerIframe);
+	  
+	  // Verify banner image
+	  List <WebElement> bannerImages = driver.findElements(By.xpath("//div[@id='productcontainer']//img"));
+	  int bannerImageNumber = bannerImages.size();
+	  
+	  // Check image =6
+	  Assert.assertEquals(bannerImageNumber, 6);
+	  
+	  // Check all  image are displayed
+	  for(WebElement image: bannerImages) {
+		  Assert.assertTrue(image.isDisplayed());
+		  System.out.println("Images displayed!");
+	  }
+	  
+	  
   }
   
  
