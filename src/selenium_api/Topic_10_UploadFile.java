f6613e25bff35cd4cb7cf24ad603976eff35f71b
ISC-HATTM <ISC-HATTM@W010983116.officesg.local>
2018-08-30 18:08:16 +0700

    UploadFile_04

--- src/selenium_api/Topic_10_UploadFile.java
+++ src/selenium_api/Topic_10_UploadFile.java
@@ -8,6 +8,7 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -16,24 +17,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Topic_10_UploadFile {
 	WebDriver driver;
 	String projectDirectory = System.getProperty("user.dir");
-	String fileName = "Upload.png";
-	String uploadFilePath = projectDirectory + "\\imges\\" + fileName;
+	String fileName_01 = "01.png";
+	String fileName_02 = "02.png";
+	String fileName_03 = "03.png";
+	String uploadFilePath00 = projectDirectory + "\\imges\\" + fileName_01;
+	String uploadFilePath01 = projectDirectory + "\\imges\\" + fileName_02;
+	String uploadFilePath02 = projectDirectory + "\\imges\\" + fileName_03;
+	
 	String chromeUpload = projectDirectory + "\\upload\\chrome.exe";
 	String firefoxUpload = projectDirectory + "\\upload\\firefox.exe";
 	String ieUpload = projectDirectory + "\\upload\\ie.exe";
+	String folderName = "authattm" + randomNumer();
+	String email = "authattm"+ randomNumer() + "@gmail.com";
+	String name = "Automation FC";
 
 	@BeforeClass
 	public void beforeClass() {
+
 		
-		 /* System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe"); 
-		  driver = new ChromeDriver();
-		 */
+		/* System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe"); 
+		driver = new ChromeDriver();*/
+		 
 
 		//driver= new FirefoxDriver();
 
@@ -42,14 +53,15 @@ public class Topic_10_UploadFile {
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		System.out.println(uploadFilePath);
+		
 	}
 
 	public void TC_01_SendkeysAPI() {
 		driver.get("http://blueimp.github.io/jQuery-File-Upload/");
 
 		WebElement uploadElement = driver.findElement(By.xpath("//input[@type='file']"));
-		uploadElement.sendKeys(uploadFilePath);
+		String summaryFile = "";
+		uploadElement.sendKeys(uploadFilePath00);
 
 		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + fileName + "']")).isDisplayed());
 
@@ -83,13 +95,13 @@ public class Topic_10_UploadFile {
 
 		// Copy location to clipboard
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(select, null);
-		
+
 		// Chrome/ Firefox/ IE
 		WebElement uploadElement = driver.findElement(By.cssSelector(".fileinput-button"));
 		uploadElement.click();
 		Thread.sleep(3000);
-		
-		Robot robot = new Robot();	
+
+		Robot robot = new Robot();
 
 		// Focus to textbox
 		robot.keyPress(KeyEvent.VK_ENTER);
@@ -98,18 +110,17 @@ public class Topic_10_UploadFile {
 		// Giả lập phím nhấn xuống Ctrl-V
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_V);
-		
+
 		// Giả nhả phím nhấn xuống Ctrl-V ra
 		robot.keyRelease(KeyEvent.VK_CONTROL);
 		robot.keyRelease(KeyEvent.VK_V);
 		Thread.sleep(1000);
-		
-		
+
 		// Nhấn Enter
 		robot.keyPress(KeyEvent.VK_ENTER);
 		robot.keyRelease(KeyEvent.VK_ENTER);
 		Thread.sleep(2000);
-		
+
 		// verify uploaded
 		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + fileName + "']")).isDisplayed());
 		driver.findElement(By.xpath("//table//button[@class='btn btn-primary start']")).click();
@@ -117,12 +128,44 @@ public class Topic_10_UploadFile {
 	}
 
 	@Test
-	public void TC_04_Robot() {
+	public void TC_04_UploadFileChucker() {
+
+		driver.get("https://encodable.com/uploaddemo/");
+
+		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(uploadFilePath);
+
+		WebElement uploadDropdown = driver.findElement(By.xpath("//select[@name='subdir1']"));
+		Select select = new Select(uploadDropdown);
+		select.selectByVisibleText("/uploaddemo/files/");
+
+		driver.findElement(By.xpath("//input[@id='newsubdir1']")).sendKeys(folderName);
+		driver.findElement(By.xpath("//input[@id='formfield-email_address']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@id='formfield-first_name']")).sendKeys(name);
+		driver.findElement(By.xpath("//input[@id='uploadbutton']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//dl[@id='fcuploadsummary']//dd[text()='Email Address: " + email + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//dl[@id='fcuploadsummary']//dd[text()='First Name: " + name + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//dl[@id='fcuploadsummary']//dt[contains(text(),'File 1 of 1')]//a[text()='"+ fileName + "']")).isDisplayed());
+		
+		driver.findElement(By.xpath("//a[text()='View Uploaded Files']")).click();
+		driver.findElement(By.xpath("//a[text()='"+ folderName +"']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='"+ fileName + "']")).isDisplayed());
+
+		
+		
+		
+		
 		
 	}
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
 
+	public int randomNumer() {
+		Random random = new Random();
+		return random.nextInt(999999);
+	}
 }
