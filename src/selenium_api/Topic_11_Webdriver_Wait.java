12d09e245e2632c0a19d90ee7338090121a74f2c
HaTran <HaTran@DESKTOP-3GPU4AO>
2018-09-10 22:34:32 +0700

    Checking_wait_example

--- src/selenium_api/Topic_11_Webdriver_Wait.java
+++ src/selenium_api/Topic_11_Webdriver_Wait.java
@@ -2,6 +2,7 @@ package selenium_api;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -13,59 +14,63 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 
 public class Topic_11_Webdriver_Wait {
 	WebDriver driver;
 	WebDriverWait wait;
-	@BeforeClass
-	  public void beforeClass() {
-		 
-		  
-	  }
+	String email = "mongha2403@gmail.com", password = "123456";
 	
-  @Test
-  public void TC_01_ImplicitWait() {
-	  driver= new FirefoxDriver();
-	  wait =  new WebDriverWait(driver,30);
-	  driver.manage().timeouts().implicitlyWait(02, TimeUnit.SECONDS);
-	  driver.manage().window().maximize();
-	  
-	 WebElement startButton = driver.findElement(By.xpath("//button[contains(text(),'Start')]"));
-	 startButton.click();
-	 
-	 WebElement helloText = driver.findElement(By.xpath("//h4[text()='Hello World!']"));
-	 String text = helloText.getText();
-	 
-	// Wait for element/elements visible
-	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("")));
 
-	  // Wait for element/elements presence
-	  wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));
+	@BeforeMethod
+	public void Method() {
+		driver = new FirefoxDriver();
+		wait = new WebDriverWait(driver, 10);
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 
-	  // Wait for element/elements invisible
-	  wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("")));
-	  
-	  List <WebElement> elements = driver.findElements(By.xpath(""));
-	  wait.until(ExpectedConditions.invisibilityOfAllElements(elements));
+	}
 
-	  // Wait for element/elements clickable
-	  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("")));
-
-	  // Wait for an Alert presence
-	  wait.until(ExpectedConditions.alertIsPresent());
-	 
-	 
-  }
-  
-  @Test
-  public void TC_02() {
+	@Test
+	public void TC_01_ImplicitWait() {
 	
-  }
-  
 
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+		// Wait for element/elements visible-> failed
+		driver.get("http://live.guru99.com/index.php/customer/account/login/");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		driver.findElement(By.xpath("//a/span[text()='Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Log Out']")).click(); // 0.5s
+		// 5s sau nó mới load ra ngoài Homepage lại
+		driver.findElement(By.xpath("//img[contains(@src,'logo.png')]")).click();
+
+	}
+
+	@Test
+	public void TC_02() {
+		
+		
+		driver.get("http://live.guru99.com/index.php/customer/account/login/");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@id='send2']")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a/span[text()='Account']")));
+		driver.findElement(By.xpath("//a/span[text()='Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[contains(@src,'logo.png')]")));
+		driver.findElement(By.xpath("//img[contains(@src,'logo.png')]")).click();
+		
+
+		
+		
+
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
 
 }
