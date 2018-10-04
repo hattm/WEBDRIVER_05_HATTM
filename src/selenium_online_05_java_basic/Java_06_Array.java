143be2aa4e7a5c3c1dd2e8108982a0dad1468faa
ISC-HATTM <ISC-HATTM@W010983116.officesg.local>
2018-10-04 15:33:11 +0700

    Array

---
+++ src/selenium_online_05_java_basic/Java_06_Array.java
@@ -0,0 +1,13 @@
+package selenium_online_05_java_basic;
+
+public class Java_06_Array {
+
+	public static void main(String[] args) {
+		String[] testingPart = new String[4];
+		testingPart[0] = "Automation Testing";
+		testingPart[1] = "Manual Testing";
+		testingPart[2] = "Perfomance Testing";
+		testingPart[3] = "Security Testing";
+	}
+	
+}
