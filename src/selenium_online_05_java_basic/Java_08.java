8afd0d834748dd0a9b5d86e9b3c1ad612f250a76
HaTran <HaTran@DESKTOP-3GPU4AO>
2018-10-10 22:55:28 +0700

    Check_java

---
+++ src/selenium_online_05_java_basic/Java_08.java
@@ -0,0 +1,29 @@
+package selenium_online_05_java_basic;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Java_08 {
+	WebDriver driver;
+	@BeforeClass
+		public void beforeClass() {
+			
+		}
+		
+		@Test
+		public void TC_01() {
+			
+	
+		}
+		
+		@AfterClass
+		public void afterClass() {
+			
+		}
+
+}
