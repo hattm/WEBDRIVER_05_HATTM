5b8c150645fcba83d0b83c86a499840bf4186234
HaTran <HaTran@DESKTOP-3GPU4AO>
2018-10-15 22:38:07 +0700

    Checking_String

--- src/selenium_online_05_java_basic/Java_07_String.java
+++ src/selenium_online_05_java_basic/Java_07_String.java
@@ -3,10 +3,11 @@ package selenium_online_05_java_basic;
 public class Java_07_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String automation = " Automation Test Online ";
 		
-		/*=============Length===============*/
+		String testing = "Automation Testing Online Advance 05";
+		String automation = "Automation Testing Online ";
+		
+		//=============Length===============
 		int length = automation.length();
 		System.out.println("Do dai cua chuoi = " + length);
 		
@@ -15,18 +16,105 @@ public class Java_07_String {
 		char kytu = chuoi.charAt(5);
 		System.out.println("Ky tu = "+ kytu);
 		
-		String testing = automation.concat("Tutorial");
-		System.out.println("Noi chuoi = "+ testing);
+		String test= automation.concat("Tutorial");
+		System.out.println("Noi chuoi = "+ test);
 		
 		
 		
-		//Automation Test Online
+		// Automation Test Online
 		// Automation Testing Tutorial
-		boolean compareValue = automation.equals(automation);
-		System.out.println("So sanh chuoi = "+ compareValue);
+		// so sanh chuổi tuyệt đối
+		boolean compareValue = testing.equals(automation);
+		System.out.println("So sanh chuoi tuyệt đối = " + compareValue);
 		
 		compareValue = automation.equals(automation);
-		System.out.println("So sanh chuoi = "+ compareValue);
+		System.out.println("So sanh chuoi tuyệt đối = " + compareValue);
+		
+		// so sánh chuổi tuyệt đối
+		compareValue = testing.contains("Test Online");
+		System.out.println("So sanh chuoi tương đối = " + compareValue);
+		
+		
+		// testing = Automation Testing Online
+		int index_01 = testing.indexOf("Online");
+		/*int index_02 = testing.indexOf("Advance");
+		System.out.println("Vị trí chuôi = "+ index_01);
+		System.out.println("Vị trí chuôi = "+ index_02);*/
+		
+		
+		String subStringStart = testing.substring(index_01);
+		System.out.println("Chuoi con = "+ subStringStart);
+		
+		String suStringToEnd = testing.substring(11,18);
+		System.out.println("Chuoi con = "+ suStringToEnd);
+		
+		suStringToEnd = testing.substring(19,25);
+		System.out.println("Chuoi con = "+ suStringToEnd);
+		
+		
+		// Viewing 36 of 12591 results ham tach chuoi
+		String text = " Viewing 36 of 12591 results ";
+		text = text.trim();
+		String[] subString = text.split(" ");
+		for(int i = 0; i< subString.length; i++) {
+			System.out.println("Vi tri thu " + i + " : " + subString[i]);
+		}
+		System.out.println(extractNumberFromString(text,1));
+		System.out.println(extractNumberFromString(text,3));
+		
+		String price = "$500.00";
+		String url = "http://live.guru99.com/index.php/mobile.html";
+		price = price.replace("$", "");
+		price = price.replace(".00", "");
+		url = url.replace("http:", "");
+		url = url.replace("https:", "");
+		url = url.replace("//", "");
+		url = url.replace("$", "");
+		url = url.replace("!", "");
+		url = url.replace("/", "");
+		System.out.println(price);
+		System.out.println(url);
+		
+		
+		String upper = testing.toUpperCase();
+		System.out.println("In hoa = " + upper);
+		
+		String lower = testing.toLowerCase();
+		System.out.println("In thuong = " + lower);
+		
+		 String trimString  = " "
+				
+				+ "    "
+				+ "    "
+				+ "    Viewing 36 of 12591 results ham tach chuoi     "
+				+ "     "
+				+ "     ";
+		System.out.println(trimString);
+		trimString = trimString.trim();
+		System.out.println(trimString);
+		
+		String giaTien = "$500.00";
+		giaTien = giaTien.replace("$", "");
+		giaTien = giaTien.replace(".00", "");
+		int tien = Integer.parseInt(giaTien);
+		System.out.println("String = " + giaTien);
+		System.out.println("Integer = " + tien);
+		
+		double tienDouble = Double.parseDouble(giaTien);
+		System.out.println("Double = " + tienDouble);
+		
+		
+		float tienFloat  = Float.parseFloat(giaTien);
+		System.out.println("Float = " + tienFloat);
+
+		
 	}
+	
+	public static int extractNumberFromString(String text, int number) {
+		String[] subString = text.split(" ");
+		int result = Integer.parseInt(subString[number]);
+		return result;
+	}
+		
 
 }
---
+++ src/selenium_online_05_java_basic/Java_Exercise.java
@@ -0,0 +1,46 @@
+package selenium_online_05_java_basic;
+
+public class Java_Exercise {
+	public static void main(String[] args) {
+		String testing2 = "Automation Testing Tutorials Online";
+		String testing = "Automation 01 Testing 345 Tutorials Online 1234565";
+
+		char kyTu = 'a';
+		int count = 0;
+		// duyệt từ đầu đến cuối chuỗi
+		for (int i = 0; i < testing.length(); i++) {
+			// Nếu ký tự tại vị trí thứ i bằng 'a' thì tăng count lên 1
+			if (testing.charAt(i) == kyTu) {
+				count++;
+			}
+		}
+
+		System.out.println("Số lần xuất hiện của ký tự " + kyTu + " trong chuỗi " + testing + " = " + count);
+
+		boolean compareValue = testing.contains("Testing");
+		System.out.println("So sanh chuoi tương đối = " + compareValue);
+
+		boolean StringStart = testing.startsWith("Automation");
+		System.out.println("Chuoi bat dau bang Automation = " + StringStart);
+
+		boolean StringEnd = testing.endsWith("Online");
+		System.out.println("Chuoi ket thuc bang Online = " + StringEnd);
+
+		// Truyền vào chuỗi con và chỉ số bắt đầu
+		int index3 = testing2.indexOf("Tutorials");
+		System.out.println("Vi tri tu Tutorials = " + index3);
+
+		String replaceString = testing.replace("Online", "Ofline");// thay thế tất cả chuỗi "is" to "was"
+		System.out.println(replaceString);
+
+		int digitCount = 0;
+		for (int i = 0; i < testing.length(); i++) {
+			if (Character.isDigit(testing.charAt(i)))
+				digitCount++;
+		}
+
+		System.out.println("So chu so: " + digitCount);
+
+	}
+
+}
